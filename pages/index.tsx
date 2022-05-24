ec9698c96cb31828bc91763ac087c5a0c831a640
mr-m3l3 <a.m3l3@protonmail.com>
2022-05-25 09:47:03 +1000

    add graphql && gh api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,37 @@
 import React from "react";
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
 
 export default function Home() {
   return <div></div>;
+}
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query ($userName: String!) {
+        user(login: $userName) {
+          contributionsCollection {
+            contributionCalendar {
+              totalContributions
+              weeks {
+                contributionDays {
+                  contributionCount
+                  date
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      userName: "mr-m3l3",
+    },
+  });
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
