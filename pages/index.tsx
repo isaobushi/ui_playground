7b4449f67dadd48735b89276998384e3d7852553
mr-m3l3 <a.m3l3@protonmail.com>
2022-05-25 11:54:36 +1000

    add graph

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,80 @@ import React from "react";
 import { gql } from "@apollo/client";
 import client from "../apollo-client";
 
-export default function Home() {
-  return <div></div>;
+interface IDataProps {
+  data: [
+    {
+      __typename: string;
+      contributionDays: [
+        {
+          __typename: string;
+          date: string;
+          contributionCount: number;
+        }
+      ];
+    }
+  ];
+}
+export default function Home(props: IDataProps) {
+  // function that check previous value then assign color
+  const { data } = props;
+  const checkColor = (value: number, previousValue: number) => {
+    if (value > previousValue) {
+      return "darkgreen";
+    } else if (value === 0) {
+      return "grey";
+    } else {
+      return "yellow";
+    }
+  };
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center", width: "90%" }}>
+      {data.map((week) => {
+        const { contributionDays } = week;
+        return (
+          <div
+            key={Math.random()}
+            style={{
+              display: "flex",
+              border: "1px solid grey",
+              padding: "3px",
+            }}
+          >
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              {contributionDays.map((day, i) => {
+                const { contributionCount } = day;
+                const previousDayCount = contributionDays[i - 1]
+                  ? contributionDays[i - 1].contributionCount
+                  : 0;
+                return (
+                  <div
+                    key={Math.random()}
+                    style={{
+                      height: "10px",
+                      width: "10px",
+                      backgroundColor: checkColor(
+                        day.contributionCount,
+                        previousDayCount
+                      ),
+                      textAlign: "center",
+                      padding: "3px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      margin: "2px",
+                    }}
+                  >
+                    {contributionCount}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 export async function getStaticProps() {
   const { data } = await client.query({
@@ -31,7 +103,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      data,
+      data: data.user.contributionsCollection.contributionCalendar.weeks,
     },
   };
 }
